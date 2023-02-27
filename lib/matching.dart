b9c641d991ced19b2b1af36519d83772c8d7c7dc
최재민 <panzzang518@naver.com>
2023-02-27 16:47:34 +0900

    feat: 매칭 리스트 표기 항목에 생성일자 및 현황 표시

--- lib/matching.dart
+++ lib/matching.dart
@@ -6,6 +6,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dio/dio.dart';
+import 'package:transition/transition.dart';
+import 'package:intl/intl.dart';
 
 class MatchingPage extends StatefulWidget {
   const MatchingPage({super.key});
@@ -18,7 +20,7 @@ class MatchingPage extends StatefulWidget {
 
 class _MatchingPage extends State<MatchingPage> {
   late MatchingModel matchingModel;
-  List<dynamic> matchings = [];
+  List<MatchingModel> matchings = [];
   List<Image> matchingImages = [];
   Future<Map<String, String>> getHeaders() async {
     final prefs = await SharedPreferences.getInstance();
@@ -33,45 +35,60 @@ class _MatchingPage extends State<MatchingPage> {
         padding: const EdgeInsets.all(3.0),
         child: Row(
           children: [
-            Row(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.black12,
-                    image: DecorationImage(
-                        image: matchingImages[i].image, fit: BoxFit.cover),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.black12,
+                      image: DecorationImage(
+                          image: matchingImages[i].image, fit: BoxFit.cover),
+                    ),
+                    height: 100,
+                    width: 100,
                   ),
-                  height: 100,
-                  width: 100,
-                ),
-                Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Container(
-                      child: Text(
-                        "${matchings[i]['itemName']}",
-                      ),
-                    )),
-              ],
+                  Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Container(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "${matchings[i].itemName}",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              DateFormat.yMMMd('en_US')
+                                  .format(matchings[i].createdTime),
+                              style: TextStyle(
+                                  color: Colors.black45, fontSize: 15),
+                            )
+                          ],
+                        ),
+                      )),
+                ],
+              ),
             ),
-            SizedBox(
-              height: 100,
-              width: 150,
-            ),
-            Row(
-              children: [
-                Text(matchings[i]['status']),
-                Icon(
-                  Icons.adjust_rounded,
-                  color: matchings[i]['status'] == "WAIT"
-                      ? Colors.orange
-                      : Colors.red,
-                ),
-              ],
+            Container(
+              alignment: Alignment.topRight,
+              child: Row(
+                children: [
+                  Text(matchings[i].status.name),
+                  Icon(Icons.adjust_rounded,
+                      color: colors[matchings[i].status]),
+                ],
+              ),
             ),
           ],
         ));
   }
+
+  Map<Status, Color> colors = {
+    Status.REJECTED: Colors.red,
+    Status.WAIT: Colors.orange,
+    Status.CONFIRMED: Colors.green
+  };
 
   ListView getListView() => ListView.separated(
         itemCount: matchings.length,
@@ -81,12 +98,11 @@ class _MatchingPage extends State<MatchingPage> {
               onTap: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(
-                    builder: (context) => MatchingDetailScreen(
-                      matchingModel:
-                          MatchingModel.fromJson(matchings[position]),
-                    ),
-                  ),
+                  Transition(
+                      child: MatchingDetailScreen(
+                        matchingModel: matchings[position],
+                      ),
+                      transitionEffect: TransitionEffect.BOTTOM_TO_TOP),
                 );
               });
         },
@@ -138,10 +154,9 @@ class _MatchingPage extends State<MatchingPage> {
     };
     List<Image> imageList = [];
 
-    var dataURL =
-        Uri.http("10.0.2.2:8080", "/matching/status", queryParameters);
+    var dataURL = Uri.http("10.0.2.2:8080", "/matching");
     http.Response response = await http.get(dataURL, headers: header);
-    List body = jsonDecode(response.body)['content'];
+    List body = jsonDecode(utf8.decode(response.bodyBytes));
     final dio = Dio();
     for (var element in body) {
       final response = await dio.get(
@@ -150,8 +165,13 @@ class _MatchingPage extends State<MatchingPage> {
       Image image = Image.memory(response.data);
       imageList.add(image);
     }
+    List<MatchingModel> matchingList = body
+        .map(
+          (e) => MatchingModel.fromJson(e),
+        )
+        .toList();
     setState(() {
-      matchings = body;
+      matchings = matchingList;
       matchingImages = imageList;
     });
   }
--- lib/matching_model.dart
+++ lib/matching_model.dart
@@ -7,19 +7,25 @@ class MatchingModel {
   final String? rejectMessage;
   final String itemName;
   final Status status;
+  final String memberName;
+  final DateTime createdTime;
 
   MatchingModel(
       {required this.id,
       this.rejectMessage,
       required this.itemName,
-      required this.status});
+      required this.status,
+      required this.memberName,
+      required this.createdTime});
 
   factory MatchingModel.fromJson(Map<String, dynamic> json) {
     return MatchingModel(
         id: json["id"],
         status: Status.getByCode(json["status"]),
         itemName: json["itemName"],
-        rejectMessage: json["rejectMessage"]);
+        rejectMessage: json["rejectMessage"],
+        memberName: json["memberResponseDto"]["name"],
+        createdTime: DateTime.parse(json["createdDate"]));
   }
 }
 
