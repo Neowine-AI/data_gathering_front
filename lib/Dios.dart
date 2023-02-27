2b62542a228d9a436ae31b33646a34ff9c89b6bc
최재민 <panzzang518@naver.com>
2023-02-27 16:30:37 +0900

    feat: dio 재사용

---
+++ lib/Dios.dart
@@ -0,0 +1,27 @@
+import 'package:data_gathering/dioInterceptor.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<Dio> noAuthDio() async {
+  var dio = Dio();
+
+  dio.interceptors.clear();
+  dio.options.baseUrl = "http://10.0.2.2:8080";
+  dio.options.contentType = "application/json";
+
+  return dio;
+}
+
+Future<Dio> authDio() async {
+  var dio = Dio();
+  final prefs = await SharedPreferences.getInstance();
+
+  dio.interceptors.clear();
+  dio.options.baseUrl = "http://10.0.2.2:8080";
+  dio.interceptors.add(DioInterceptors());
+
+  dio.options.headers['Authorization'] =
+      "Bearer ${prefs.getString("accessToken")}";
+  return dio;
+}
