a0b133e4b92535c80940482ad7b3672b4cfb0a01
최재민 <panzzang518@naver.com>
2023-03-14 10:23:41 +0900

    feat: 매칭 반려 기능

--- lib/matching/matching_detail.dart
+++ lib/matching/matching_detail.dart
@@ -6,7 +6,9 @@ import 'package:data_gathering/item/item_detail.dart';
 import 'package:data_gathering/item/item_model.dart';
 import 'package:data_gathering/matching/matching_model.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_flavor/flutter_flavor.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
@@ -42,6 +44,7 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
   late ItemModel item;
   late Image itemImage;
   List<String> angles = ["front", "back", "top"];
+  late String rejectMessage;
 
   Future<Map<String, String>> getHeaders() async {
     final prefs = await SharedPreferences.getInstance();
@@ -76,7 +79,7 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
         'angle': angle,
       };
       final response =
-          await dio.get("http://dev.neowine.com/matching/image/${body['id']}",
+          await dio.get("http://data.neowine.com/matching/image/${body['id']}",
               queryParameters: queryParameters,
               options: Options(
                 responseType: ResponseType.bytes,
@@ -183,14 +186,21 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
     Navigator.pop(context);
   }
 
+  void rejectMatching(String message) async {
+    var dio = await authDio(context);
+    var response = dio.post("/matching/reject",
+        data: {"itemId": widget.matchingModel.id, "rejectMessage": message});
+    Navigator.pop(context);
+  }
+
   PopupMenuItem<MenuItems> menuItem(MenuItems menu) {
     return PopupMenuItem<MenuItems>(value: menu, child: Text(menu.name));
   }
 
-  AlertDialog getDialog(MenuItems menu, BuildContext context) {
+  Widget getDialog(MenuItems menu, BuildContext context) {
     switch (menu) {
       case MenuItems.delete:
-        return AlertDialog(
+        return CupertinoAlertDialog(
           title: Text("삭제"),
           content: Text("매칭을 삭제하시겠습니까?"),
           actions: [
@@ -208,7 +218,7 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
           ],
         );
       case MenuItems.confirm:
-        return AlertDialog(
+        return CupertinoAlertDialog(
           title: Text("승인"),
           content: Text("매칭을 승인하시겠습니까?"),
           actions: [
@@ -227,22 +237,39 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
           ],
         );
       case MenuItems.reject:
-        return AlertDialog(
-          title: Text("반려"),
-          content: Text("매칭을 반려하시겠습니까?"),
+        return CupertinoAlertDialog(
+          title: Text(
+            "반려",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: CupertinoDynamicColor.resolve(
+                    CupertinoColors.label, context),
+                fontWeight: FontWeight.w500),
+          ),
+          content: Column(children: [
+            SizedBox(height: 10),
+            Card(
+              color: Colors.transparent,
+              child: TextField(
+                onChanged: (message) => setState(() {
+                  this.rejectMessage = message;
+                }),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey.shade50,
+                  border: OutlineInputBorder(),
+                  labelText: '반려사유를 입력해주세요',
+                ),
+              ),
+            ),
+          ]),
           actions: [
             TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: Text("cancel")),
             TextButton(
-              onPressed: () {
-                Navigator.pop(context, 'OK');
-
-                //rejectMatching();
-              },
-              child: const Text('OK'),
-            ),
+                onPressed: () => rejectMatching(rejectMessage),
+                child: Text("ok"))
           ],
         );
       default:
@@ -277,8 +304,10 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
             context: context,
             builder: (context) => getDialog(menuitmes, context)),
         itemBuilder: (context) => <PopupMenuEntry<MenuItems>>[
-          menuItem(MenuItems.confirm),
-          menuItem(MenuItems.reject),
+          if (FlavorConfig.instance.variables["isAdmin"] == true)
+            menuItem(MenuItems.confirm),
+          if (FlavorConfig.instance.variables["isAdmin"] == true)
+            menuItem(MenuItems.reject),
           menuItem(MenuItems.delete)
         ],
       ),
@@ -319,79 +348,81 @@ class _MatchingDetailPage extends State<MatchingDetailPage> {
     return Scaffold(
       body: images.isEmpty
           ? Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                SizedBox(
-                  height: 30,
-                ),
-                Stack(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.width,
-                      child: getPhotoGallery(),
-                    ),
-                    BackButton(color: Colors.white),
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.width,
-                      alignment: Alignment.bottomCenter,
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        "${this.angles[currentIndex]}",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 17.0,
-                          decoration: null,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                  child: Column(children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: getItemWidget(),
-                        ),
-                        IconButton(
-                          onPressed: () => {
-                            Navigator.push(
-                              context,
-                              Transition(
-                                child: ItemDetailPage(
-                                  itemModel: item,
-                                  image: itemImage,
-                                ),
-                                transitionEffect:
-                                    TransitionEffect.BOTTOM_TO_TOP,
-                              ),
-                            )
-                          },
-                          icon: Icon(Icons.chevron_right),
-                          iconSize: 40,
-                        ),
-                      ],
-                    ),
-                    Divider(thickness: 1),
-                  ]),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    left: 10,
-                    right: 10,
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 30,
                   ),
-                  child: Stack(
+                  Stack(
                     children: [
-                      Column(
-                        children: matchingInfos,
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.width,
+                        child: getPhotoGallery(),
+                      ),
+                      BackButton(color: Colors.white),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: MediaQuery.of(context).size.width,
+                        alignment: Alignment.bottomCenter,
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          "${this.angles[currentIndex]}",
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 17.0,
+                            decoration: null,
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                    child: Column(children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: getItemWidget(),
+                          ),
+                          IconButton(
+                            onPressed: () => {
+                              Navigator.push(
+                                context,
+                                Transition(
+                                  child: ItemDetailPage(
+                                    itemModel: item,
+                                    image: itemImage,
+                                  ),
+                                  transitionEffect:
+                                      TransitionEffect.BOTTOM_TO_TOP,
+                                ),
+                              )
+                            },
+                            icon: Icon(Icons.chevron_right),
+                            iconSize: 40,
+                          ),
+                        ],
+                      ),
+                      Divider(thickness: 1),
+                    ]),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                    ),
+                    child: Stack(
+                      children: [
+                        Column(
+                          children: matchingInfos,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
     );
   }
